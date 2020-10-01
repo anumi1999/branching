e8a1e17941f323d6bf8e07ee7f279b57e7b2d4c0
sam2104 <56917667+sam2104@users.noreply.github.com>
2020-10-02 00:56:31 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+int i,n,sum=0,mean,num[100];
+
+cout<<"input the the set of numbers= ";
+
+for(i=0;i<n;i++)
+
+cin>>num[i];
+
+for(i=0;i<n;i++)
+
+sum+=num[i];
+
+cout<<"the sum is = "<<sum<<endl;
+
+return 0;
+
+}
